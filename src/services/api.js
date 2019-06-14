d90a24d14d79007373b33b1c6a428253da6c6535
Eduardo Castellini <eduardo.castellini@hotmail.com>
2019-06-14 14:30:54 -0300

    Finalizando aplicação

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://enigmatic-inlet-99957.herokuapp.com',
+    baseURL: 'http://localhost:3333',
 })
 
 export default api;
